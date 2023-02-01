a15856da680ade48fbaebf3af9bb03a945e20fec
Kang Hyomin <103807515+singrum@users.noreply.github.com>
2023-02-01 11:19:16 +0000

    x

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 function init(){
     const CHAT = document.querySelector(".chat-window");
     const INPUT = document.querySelector(".text-input");
+    const ALLWORDSDICT = {'a' : ['ab', 'bc'], 'b' : ['bc', 'ba']}
+    const HISTORY = []
     
     function loadComputerChat(text){
         CHAT.innerHTML += `<div class = "computer-chat">
@@ -9,7 +11,7 @@ function init(){
             </div>
             <div class = "nickname">끝파고</div>
             <div class = "content">
-            <span class = "talkbubble">${text}</span>
+            <div class = "talkbubble">${text}</div>
         </div>
         </div>`;
     }
@@ -17,24 +19,51 @@ function init(){
     function loadMyChat(text){
         CHAT.innerHTML += `<div class = "my-chat">
             <div class = "my-content">
-                <span class = "my-talkbubble">${text}</span>
+                <div class = "my-talkbubble">${text}</div>
             </div>
         </div>`
     }
 
-    let checkAndLoad = ()=> {if(INPUT.value !== ""){loadMyChat(INPUT.value)}; INPUT.value = ""; INPUT.click()}
+    function checkAndLoad(text){
+        if(text !== ""){
+            loadMyChat(text);
+            CHAT.scrollTop = CHAT.scrollHeight;
+            INPUT.click();
+    };}
 
-    document.querySelector(".chat-right").addEventListener("click", checkAndLoad);
-    window.addEventListener("keydown", e=> {if (e.code === "Enter"){checkAndLoad()}})
+    CHAT.addEventListener("click", ()=>{CHAT.scrollTop = CHAT.scrollHeight;})
+    document.querySelector(".chat-right").addEventListener("click", whenEntertheWord);
+    window.addEventListener("keydown", e=> {if (e.code === "Enter"){whenEntertheWord()}})
     
 
 
     function loadInitChat(){
-        loadComputerChat("안녕하세요.<br>저는 끝말잇기 인공지능 끝파고입니다.<br>데이터베이스는 (구)표준국어대사전을 기준으로 하며 먼저 단어를 제시해주면 됩니다.")
+        loadComputerChat("안녕하세요.<br>끝말잇기 인공지능 끝파고입니다.<br>먼저 단어를 제시해주세요!")
     }
     loadInitChat()
 
 
+    
+    function whenEntertheWord(){
+        let word = INPUT.value
+        checkAndLoad(word);
+        INPUT.value = "";
+        if(isInvalid(word)){
+            loadComputerChat("존재하지 않는 단어입니다!")
+        }
+        else if(isUsed(word)){
+            
+        }
+    }
+
+    function isInvalid(word){
+        if(ALLWORDSDICT[word[0]] === undefined){return true};
+        if(!ALLWORDSDICT[word[0]].includes(word)){return true};
+        return false;
+    }
+    function isUsed(word){
+        
+    }
 
 
 }
